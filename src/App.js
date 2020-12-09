8780251db70b2eb5e734c5bce7aebfae462a3cee
KumarAtmish <73941481+KumarAtmish@users.noreply.github.com>
2020-12-09 23:20:46 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./styles.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle";
+import Home from "./Home";
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
+import About from "./About";
+import Contact from "./Contact";
+import Login from "./Login";
+import Navbar from "./Navbar";
+
+export default function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/contact" component={Contact} />
+          <Route exact path="/login" component={Login} />
+          <Redirect to="/" />
+        </Switch>
+      </BrowserRouter>
+    </>
+  );
+}
